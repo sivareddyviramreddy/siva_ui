ba72cf7112934d494ac9a9caa0db5bde113df79e
sivareddyviramreddy <88603662+sivareddyviramreddy@users.noreply.github.com>
2021-11-24 12:25:53 +0530

    Revert "added reactcomponent"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactformComponent } from './reactform/reactform.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ReactformComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
